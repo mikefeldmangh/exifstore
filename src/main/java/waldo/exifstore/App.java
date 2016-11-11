043be89e8da72a050086867465406577e9e3e0ca
Michael Feldman <mikesinbox111@gmail.com>
2016-11-11 14:47:23 -0500

    Add comments to main class.

--- src/main/java/waldo/exifstore/App.java
+++ src/main/java/waldo/exifstore/App.java
@@ -1,7 +1,11 @@
 package waldo.exifstore;
 
 /**
- * Hello world!
+ * This application reads in photos from an S3 network store, parses the EXIF data from
+ * the photos and writes the EXIF information to a local Elastic Search store.
+ * 
+ * In the future the application can be expanded to include the ability to read from and write to
+ * other types of stores.
  *
  */
 public class App 

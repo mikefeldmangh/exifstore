4c692c12d943b9ee51adb33dca27e2efa837f3e3
Michael Feldman <mikesinbox111@gmail.com>
2016-11-12 10:58:00 -0500

    Create interface for writing metadata to a store.

---
+++ src/main/java/waldo/exifstore/exifwriter/MetadataWriter.java
@@ -0,0 +1,17 @@
+package waldo.exifstore.exifwriter;
+
+import com.drew.metadata.Metadata;
+
+/**
+ * This interface defines the methods available for writing metadata to a store.
+ * @author michaelfeldman
+ *
+ */
+public interface MetadataWriter {
+
+	/**
+	 * Writes the given metadata to a local store. The local store will be configured in each implementation.
+	 * @param metadata
+	 */
+	public void writeMetadataToStore(Metadata metadata);
+}
